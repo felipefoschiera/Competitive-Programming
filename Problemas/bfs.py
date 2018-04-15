a0c47a81cfd85dedf9b49fbf0aa9a539977d2198
felipefoschiera <felipefoschiera@googlemail.com>
2018-04-15 18:03:45 -0300

    Update

--- Problemas/bfs.py
+++ Problemas/bfs.py
@@ -14,7 +14,6 @@ class Graph:
         visited[s] = True
         while queue:
             s = queue.pop(0) 
-            print('Visitado:', s)
             for i in self.graph[s]:
                 if visited[i] == False:
                     queue.append(i)
