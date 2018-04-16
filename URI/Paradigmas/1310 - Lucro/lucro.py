a950a79513a7bdd261d01686d932034a7b88d78c
felipefoschiera <felipefoschiera@googlemail.com>
2018-04-16 10:26:57 -0300

    Lucro

---
+++ URI/Paradigmas/1310 - Lucro/lucro.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8
+# Felipe G. Foschiera
+# URI 1310 - Lucro
+# Maximum subarray sum - direto da entrada, sem colocar em array
+while True:
+    try:
+        N = int(input())
+        custo_dia = int(input())
+        soma = best = 0
+        for _ in range(N):
+            receita = int(input())
+            lucro = receita - custo_dia            
+            soma = max(lucro, soma+lucro)
+            best = max(soma, best)
+        print(best)
+    except EOFError:
+        break
