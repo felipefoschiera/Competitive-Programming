ce29e7d0332eb9d47c7eb019eab00249fbb8a885
felipefoschiera <felipefoschiera@googlemail.com>
2018-04-16 19:38:34 -0300

    Update

---
+++ Funcoes/Hex para String.py
@@ -0,0 +1,2 @@
+hex_string = "73 6F 63 6F 72 72 6F"
+string = bytearray.fromhex(hex_string).decode()
---
+++ URI/AD-HOC/1986 - Perdido em Marte/marte.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Felipe G. Foschiera
+# URI 1986 - Perdido em Marte
+n = int(input())
+palavras = input()
+print(bytearray.fromhex(palavras).decode())
