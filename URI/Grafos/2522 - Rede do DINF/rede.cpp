27cf36438f6c881cb8713b11a28d207582c27d2b
Felipe Foschiera <felipefoschiera@googlemail.com>
2018-05-14 18:57:18 -0300

    Update

---
+++ URI/Grafos/2522 - Rede do DINF/rede.cpp
@@ -0,0 +1,66 @@
+/**
+ *  URI 2522 - Rede do DINF
+ *  Felipe G. Foschiera
+ */
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef pair<double, ii> dii;
+
+double distance(ii a, ii b){
+    return hypot(a.first-b.first, a.second-b.second);
+}
+
+vector<int> pai;
+
+int findSet(int x){
+    return (pai[x] == x) ? x : pai[x] = findSet(pai[x]);
+}
+
+bool isSameSet(int a, int b){
+    return findSet(a) == findSet(b);
+}
+
+void unionSet(int a, int b){
+    pai[findSet(a)] = findSet(b);
+}
+
+int main(){
+    int N;
+    while(scanf("%d", &N) != EOF){
+        vector<ii> pontos;
+        vector<dii> arestas;
+        int a, b;
+        for(int i = 0; i < N; i++){
+            scanf("%d %d", &a, &b);
+            pontos.push_back(ii(a, b));
+        }
+        for(int i = 0; i < N; i++){
+            ii pa = pontos[i];
+            for(int j = 0; j < i; j++){
+                ii pb = pontos[j];
+                double dist = distance(pa, pb);
+                arestas.push_back(dii(dist, ii(i, j)));
+            }
+        }
+        double total = 0.0;
+        sort(arestas.begin(), arestas.end());
+        pai.assign(N, 0);
+        for(int i = 0; i < N; i++) pai[i] = i;    
+        for(auto e : arestas){
+            if(!isSameSet(e.second.first, e.second.second)){
+                unionSet(e.second.first, e.second.second);
+                total += e.first;
+            }
+        }  
+        printf("%.2lf\n", total);
+        arestas.clear();
+        pai.clear();
+        pontos.clear();  
+    }
+    return 0;
+}
