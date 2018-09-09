b4a11a411f00c16d2a33c4b65aef22a39d16feb9
felipefoschiera <felipefoschiera@googlemail.com>
2018-09-09 20:58:28 -0300

    Update

--- URI Online Judge/Estruturas e Bibliotecas/2658 - Acordes Intergaláticos/acordes.cpp
+++ URI Online Judge/Estruturas e Bibliotecas/2658 - Acordes Intergaláticos/acordes.cpp
@@ -1,104 +1,103 @@
 #include <stdio.h>
-#include <vector>
-#include <string>
-#include <string.h>
-#define MAX 102345
+#include <cstring>
+#define MAX 100005
 using namespace std;
 
-struct frequencia {
-    int freq[9];
-    
-    void shift(int n){
-        int temp[9];
-        for(int i = 0; i < 9; i++) temp[i] = freq[i];
-        for(int i = 0; i < 9; i++) {
-            freq[i] = temp[(i-n+9)%9];
-        }
-    }
 
-    int maximo() {
-        int res = 0;
-        for(int i = 0; i < 9; i++) if(freq[i] >= freq[res]) res = i;
-        return res;
-    }
-};
-
-frequencia operator + (const frequencia a, const frequencia b){
-    frequencia nova;
-    for(int i = 0; i < 9; i++) nova.freq[i] = a.freq[i] + b.freq[i];
-    return nova;
-}
-
+int queryInterval[9];
 
 class SegmentTree {
 private:
-    vector<int> lazy;
-    frequencia st[4*MAX];
+    int st[4*MAX][9];
+    int lazy[4*MAX];
+    int temp[9];
     int size;
 #define left(p) (p << 1)
 #define right(p) ((p << 1) + 1)
-    void build(int p, int l, int r, int *A){
+    void build(int p, int l, int r){
         if(l == r){
-            st[p].freq[A[l]] = 1;
-        }else {
-            build(left(p), l, (l+r)/2, A);
-            build(right(p), (l+r)/2+1, r, A);
-            st[p] = st[left(p)] + st[right(p)];
+            st[p][1] = 1;
+        }else{
+            build(left(p), l, (l+r)/2);
+            build(right(p), (l+r)/2+1, r);
+            st[p][1] = st[left(p)][1] + st[right(p)][1];
         }
     }
     void push(int p, int l, int r){
-        st[p].shift(lazy[p]);
+        for(int i = 0; i < 9; i++) temp[i] = st[p][i];
+        for(int i = 0; i < 9; i++) st[p][i] = temp[(i-lazy[p]+9)%9];
         if(l != r){
             lazy[right(p)] = (lazy[right(p)] + lazy[p]) % 9;
             lazy[left(p)] = (lazy[left(p)] + lazy[p]) % 9;
         }
         lazy[p] = 0;
     }
-    frequencia query(int p, int l, int r, int a, int b){
+    void query(int p, int l, int r, int a, int b){
         push(p, l, r);
-        if(a > r || b < l) return frequencia();
-        if(l >= a && r <= b) return st[p];
-        frequencia p1 = query(left(p), l, (l+r)/2, a, b);
-        frequencia p2 = query(right(p), (l+r)/2+1, r, a, b);
-        return p1 + p2;
+        if(a > r || b < l) return;
+        if(l >= a && r <= b){
+            for(int i = 0; i < 9; i++) queryInterval[i] += st[p][i];
+        }else{
+            query(left(p), l, (l+r)/2, a, b);
+            query(right(p), (l+r)/2+1, r, a, b);
+        }
     }
+
     void update(int p, int l, int r, int a, int b, int k){
         push(p, l, r);
         if(a > r || b < l) return;
-        else if(l >= a && r <= b) {
-            lazy[p] = k;
+        if(l >= a && r <= b) {
+            lazy[p] += k;
             push(p, l, r);
         }else {
             update(left(p), l, (l+r)/2, a, b, k);
             update(right(p), (l+r)/2+1, r, a, b, k);
-            st[p] = st[left(p)] + st[right(p)];
+            for(int i = 0; i < 9; i++)
+                st[p][i] = st[left(p)][i] + st[right(p)][i];
+            
         }
     }
 public:
-    SegmentTree(int *arr, int *end){
-        size = (int)(end-arr);
+    void init(int N){
+        size = N;
         memset(st, 0, sizeof st);
-        lazy.assign(4*size, 0);
-        build(1, 0, size-1, arr);
+        memset(lazy, 0, sizeof lazy);
+        build(1, 0, size-1);
     }
-    frequencia query(int a, int b){  return query(1, 0, size-1, a, b); }
+    void query(int a, int b){
+        memset(queryInterval, 0, sizeof queryInterval);
+        query(1, 0, size-1, a, b);
+    }
     void update(int a, int b, int k) { update(1, 0, size-1, a, b, k); }
 };
 
-int orig[MAX];
+
+SegmentTree st;
 
 int main(){
     int N, Q;
     while(scanf("%d %d", &N, &Q) != EOF){
-        for(int i = 0; i < N; i++) orig[i] = 1;
-        SegmentTree st(orig, orig+N);
+        st.init(N);
         int a, b;
         while(Q--){
             scanf("%d %d", &a, &b);
-            int mostfreq = st.query(a, b).maximo();
+            st.query(a, b);
+            int mostfreq = 0;
+            for(int k = 0; k < 9; k++)
+                if(queryInterval[k] >= queryInterval[mostfreq])
+                   mostfreq = k;
+
             st.update(a, b, mostfreq);
         }
-        for(int i = 0; i < N; i++) printf("%d\n", st.query(i, i).maximo());
+        for(int i = 0; i < N; i++){
+            st.query(i, i);
+            int max = 0;
+            for(int j = 0; j < 9; j++){
+                if(queryInterval[j] > queryInterval[max])
+                    max = j;
+            }
+            printf("%d\n", max);
+        }
     }
     return 0;
 }
