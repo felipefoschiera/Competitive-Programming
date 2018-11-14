32effe8b226527806d6f905b4b26a52a0492ad7e
felipefoschiera <felipefoschiera@googlemail.com>
2018-11-14 18:42:33 -0200

    Update

--- URI Online Judge/AD-HOC/2905 - Cheap Trips/uri2905.cpp
+++ URI Online Judge/AD-HOC/2905 - Cheap Trips/uri2905.cpp
@@ -9,7 +9,7 @@ const int MAXN = 10101;
 const int MAXT = 131;
 const int MAXA = 7;
 
-double pd[MAXN][MAXT][MAXA];
+double pd[MAXN][MAXA];
 
 struct trip {
 	int time;
@@ -20,8 +20,8 @@ int N;
 trip trips[MAXN];
 
 double solve(int trip, int curtime, int acum){
-	if(pd[trip][curtime][acum] != -1.0)
-		return pd[trip][curtime][acum];
+	if(pd[trip][acum] != -1.0)
+		return pd[trip][acum];
 	if(trip == N) return 0.0;
 	double res = 0.0;
 	if(acum == 0) res += trips[trip].cost;
@@ -31,19 +31,17 @@ double solve(int trip, int curtime, int acum){
 		res += min(solve(trip+1, min(curtime+trips[trip+1].time, 120), acum+1),
 				   solve(trip+1, min(trips[trip+1].time, 120), 0));
 	}else res += solve(trip+1, min(trips[trip+1].time, 120), 0);
-	return pd[trip][curtime][acum] = res;
+	return pd[trip][acum] = res;
 }
 
 int main(){
 	while(scanf("%d", &N) != EOF){
-		int d, c;
 		for(int i = 0; i < N; i++){
 			scanf("%d %d", &trips[i].time, &trips[i].cost);			
 		}
 		for(int i = 0; i < MAXN; i++)
-			for(int j = 0; j < MAXT; j++)
 				for(int a = 0; a < 7; a++)
-						pd[i][j][a] = -1.0;
+						pd[i][a] = -1.0;
 		printf("%.2lf\n", solve(0, min(trips[0].time, 120), 0));
 	}
 	return 0;
