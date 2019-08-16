51b15325bd7e0ac70f957138bdf706c3280f09a5
Felipe Foschiera <38386506+felipefoschiera@users.noreply.github.com>
2019-08-16 20:33:18 -0300

    Update Devmates-Problem1608-uber.cpp

--- Outros/Devmates-Problem1608-uber.cpp
+++ Outros/Devmates-Problem1608-uber.cpp
@@ -1,49 +1,47 @@
-#include <bits/stdc++.h>
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
 
-string s = "123456789";
+    string s = "123456789";
 
-set<string> solutions;
+    set<string> solutions;
 
-void solve(int idx, string str){
-    if(idx == 9){
-        cnter++;
-        // check if generated string is valid
-        int sum = 0, cursum = 0;
-        bool sign = true;
-        for(char x : str){
-            if(x >= '0' && x <= '9'){
-                cursum = cursum * 10 + (x - '0');
-            }else{
+    void solve(int idx, string str){
+        if(idx == 9){
+            // check if generated string is valid
+            int sum = 0, cursum = 0;
+            bool sign = true;
+            for(char x : str){
+                if(x >= '0' && x <= '9'){
+                    cursum = cursum * 10 + (x - '0');
+                }else{
+                    if(!sign) cursum = -cursum;
+                    sum += cursum;
+                    cursum = 0;
+                    sign = (x == '+');
+                }
+            }
+            if(cursum > 0){
                 if(!sign) cursum = -cursum;
                 sum += cursum;
-                cursum = 0;
-                sign = (x == '+');
             }
+            if(sum == 100){
+                if(str[0] == '+')
+                    str.erase(str.begin(), str.begin()+1);
+                solutions.insert(str);
+            }
+            return;
         }
-        if(cursum > 0){
-            if(!sign) cursum = -cursum;
-            sum += cursum;
-        }
-        if(sum == 100){
-            if(str[0] == '+')
-                str.erase(str.begin(), str.begin()+1);
-            solutions.insert(str);
-        }
-        return;
+
+        solve(idx+1, str + s[idx]); // next number
+        solve(idx+1, str + '+' + s[idx]); // add
+        solve(idx+1, str + '-' + s[idx]); // subtract
+
     }
 
-    int k = s[idx] - '0';
-    solve(idx+1, str + s[idx]); // next number
-    solve(idx+1, str + '+' + s[idx]); // add
-    solve(idx+1, str + '-' + s[idx]); // subtract
-
-}
-
-int main(){
-    solve(0, "");
-    for(string s : solutions){
-        cout << s << '\n';
+    int main(){
+        solve(0, "");
+        for(string s : solutions){
+            cout << s << '\n';
+        }
+        return 0;
     }
-    return 0;
-}
