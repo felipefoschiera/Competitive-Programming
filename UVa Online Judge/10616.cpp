fe664046d0161fd6a15d4b8d8f983e77c6dc3dcb
Felipe Foschiera <38386506+felipefoschiera@users.noreply.github.com>
2019-10-04 16:22:22 -0300

    Update 10616.cpp

--- UVa Online Judge/10616.cpp
+++ UVa Online Judge/10616.cpp
@@ -1,35 +1,38 @@
-#include <iostream>
-#include <string.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
 
-ll vals[212];
+ll v[212];
 int n, q, d, m;
 
-ll dp[212][15][25];
-bool vis[212][15][25];
+ll pd[212][25][12];
 
-ll solve(int pos, int pegos, int sum){
+ll solve(int pos, ll sum, int pegos){
     if(pegos == m) return sum == 0;
     if(pos == n) return 0;
-    if(vis[pos][pegos][sum])
-        return dp[pos][pegos][sum];
-    vis[pos][pegos][sum] = 1;
-    ll res = solve(pos+1, pegos+1, (sum+vals[pos])%d) 
-               + solve(pos+1, pegos, sum);
-    return dp[pos][pegos][sum] = res;
+    if(pd[pos][sum][pegos] != -1)
+        return pd[pos][sum][pegos];
+    ll adsum = (sum + v[pos]) % d;
+    if(adsum < 0) adsum += d;
+    
+    ll res = solve(pos+1, adsum%d, pegos+1) 
+                + solve(pos+1, sum%d, pegos);
+    return pd[pos][sum][pegos] = res;
 }
 
 int main(){
-    int tc = 1;
+    int st = 1;
     while(cin >> n >> q, n+q){
-        for(int i = 0; i < n; i++) cin >> vals[i];
-        cout << "SET " << tc++ << ":\n";
+        for(int i = 0; i < n; i++) cin >> v[i];
+        cout << "SET " << st++ << ":\n";
         for(int qr = 1; qr <= q; qr++){
             cin >> d >> m;
-            memset(vis, 0, sizeof vis);
+            memset(pd, -1, sizeof pd);
             cout << "QUERY " << qr << ": " << solve(0, 0, 0) << '\n';
+            
         }
     }
+    
     return 0;
 }
